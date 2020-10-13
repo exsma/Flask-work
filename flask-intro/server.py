310469c18f97af2688ea570f6f305f55be7e2113
exsma <essma@outlook.com>
2020-10-13 21:39:55 +0000

    Greet and Diss web form
    co-authored-by AmyY <72002336+AmyYang1799@users.noreply.github.com>

---
+++ flask-intro/server.py
@@ -0,0 +1,118 @@
+"""Greeting Flask app."""
+
+from random import choice
+
+from flask import Flask, request
+
+# "__name__" is a special Python variable for the name of the current module
+# Flask wants to know this to know what any imported things are relative to.
+app = Flask(__name__)
+
+AWESOMENESS = [
+    'awesome', 'terrific', 'fantastic', 'neato', 'fantabulous', 'wowza',
+    'oh-so-not-meh', 'brilliant', 'ducky', 'coolio', 'incredible',
+    'wonderful', 'smashing', 'lovely']
+
+NOT_SO_AWESOME = [
+    'bleh', 'messy' ,'boring', 'terrible', 'awful','meh', 'lamio', 'grossza',  
+    'meanie', 'awkward' , 'just a little beebo']
+
+@app.route('/')
+def start_here():
+    """Home page."""
+
+    return """<!doctype html> <html> 
+    <a href = "http://localhost:5000/hello">link</a>
+    Hi! This is the home page.</html>"""
+
+
+@app.route('/hello')
+def say_hello():
+    """Say hello and prompt for user's name."""
+
+    return """
+    <!doctype html>
+    <html>
+      <head>
+        <title>Hi There!</title>
+      </head>
+      <body>
+        <h1>Hi There!</h1>
+        <form action="/greet">
+          What's your name? <input type="text" name="person">
+          <input type="submit" value="Submit">
+          <select name="Compliment">
+            <option value="awesomeness">awesome</option>
+            <option value="terrific">terrific</option>
+            <option value="fantastic">fantastic</option>
+            <option value="neato">neato</option>
+            <option value="wowza">wowza</option>
+            <option value="oh-so-not-meh">oh-so-not-meh</option>
+            <option value="brilliant">brilliant</option>
+            <option value="ducky">ducky</option>
+            <option value="coolio">coolio</option>
+            <option value="incredible">incredible</option>
+            <option value="wonderful">wonderful</option>
+            <option value="smashing">smashing</option>
+            <option value="lovely">lovely</option>
+          </select>
+        </form>
+        <form action="/diss">
+          What's your name? <input type="text" name="person">
+          <input type="submit" value="Submit">
+        </form>
+      </body>
+    </html>
+    """
+
+
+@app.route('/greet')
+def greet_person():
+    """Get user by name."""
+
+    player = request.args.get("person")
+
+    compliment = request.args.get("Compliment")
+
+  
+
+    return """
+    <!doctype html>
+    <html>
+      <head>
+        <title>A Compliment</title>
+      </head>
+      <body>
+        Hi, {}! I think you're {}!
+      </body>
+    </html>
+    """.format(player, compliment)
+
+
+@app.route('/diss')
+def diss_person():
+    """Get user by name."""
+
+    player = request.args.get("person")
+
+    diss = choice(NOT_SO_AWESOME)
+
+  
+
+    return """
+    <!doctype html>
+    <html>
+      <head>
+        <title>A Diss</title>
+      </head>
+      <body>
+        Hi, {}! I think you're {}!
+      </body>
+    </html>
+    """.format(player, diss)
+
+
+if __name__ == '__main__':
+    # debug=True gives us error messages in the browser and also "reloads"
+    # our web app if we change the code.
+    app.run(debug=True, host="0.0.0.0")
